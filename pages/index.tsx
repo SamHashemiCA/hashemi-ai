06278f28e446f2ece92636daf7904937786a3681
Sam Hashemi <28886954+SamHashemiCA@users.noreply.github.com>
2022-10-09 17:28:30 -0700

    Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Github from "../components/logo/github";
-import Tweeter from "../components/logo/Tweeter";
 import Linkedin from "../components/logo/Linkedin";
 import styles from "../styles/Home.module.css";
 
@@ -11,20 +10,20 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Iman Heidari personal website</title>
+        <title>Sam Hashemi personal website</title>
         <meta
           name="description"
-          content="This is Iman Heidari's personal website"
+          content="This is Sam Hashemi's personal website"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Iman Heidari</h1>
+        <h1 className={styles.title}>Sam Hashemi</h1>
 
         <div className={styles.avatar}>
           <Image
-            src="https://avatars.githubusercontent.com/u/1315090?v=4"
+            src="https://media-exp1.licdn.com/dms/image/C5103AQHHaACLQZImXQ/profile-displayphoto-shrink_400_400/0/1516559447278?e=1671062400&v=beta&t=tH2xjxEA6NUVmNc7ULbPcHX9wiO_Wi9mKZbC_tPoo9U"
             alt="iman heidari"
             width={100}
             height={100}
@@ -32,13 +31,10 @@ const Home: NextPage = () => {
         </div>
 
         <div className={styles.socials}>
-          <a href="https://github.com/iheidari/">
+          <a href="https://github.com/SamHashemiCA/">
             <Github />
           </a>
-          <a href="https://twitter.com/ximaneshon/">
-            <Tweeter />
-          </a>
-          <a href="https://www.linkedin.com/in/iheidari/">
+          <a href="https://www.linkedin.com/in/samhashemi-ai/">
             <Linkedin />
           </a>
         </div>
